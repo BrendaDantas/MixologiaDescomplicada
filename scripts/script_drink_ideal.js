18519f158d5f5ced2005b1bb5fb979badc5023a3
BrendaDantas <brendasod98@gmail.com>
2024-06-21 22:58:54 -0300

    Concluindo a funcionalidade de recomendação de drinks

--- scripts/script_drink_ideal.js
+++ scripts/script_drink_ideal.js
@@ -33,7 +33,7 @@ function recommendDrink() {
                     }
                 } else {
                     if (frutasFrescas === "sim") {
-                        drinks.push("Caipirinha")
+                        drinks.push("Caipirinha", "Caipirosca")
                     } else {
                         drinks.push("Tequila Sunrise", "Daiquiri");
                     }
@@ -66,6 +66,134 @@ function recommendDrink() {
                         drinks.push("Sangria Moscato");
                     } else {
                         drinks.push("Piña Colada", "Sex on the Beach");
+                    }
+                }
+            }
+        }
+    }
+
+    if (sabor === "amargo") {
+        if (citrico === "sim") {
+            if (forcaBebida === "forte") {
+                if (gas === "sim") {
+                    if (frutasFrescas === "sim") {
+                        drinks.push("Negroni Sbagliato");
+                    } else {
+                        drinks.push("Americano");
+                    }
+                } else {
+                    if (frutasFrescas === "sim") {
+                        drinks.push("Old Fashioned com Frutas");
+                    } else {
+                        drinks.push("Boulevardier");
+                    }
+                }
+            } else {
+                if (gas === "sim") {
+                    if (frutasFrescas === "sim") {
+                        drinks.push("Amaro Spritz");
+                    } else {
+                        drinks.push("Aperol Spritz");
+                    }
+                } else {
+                    if (frutasFrescas === "sim") {
+                        drinks.push("Citrus Berry Smash")
+                    } else {
+                        drinks.push("Aperol Sour");
+                    }
+                }
+            }
+        } else {
+            if (forcaBebida === "forte") {
+                if (gas === "sim") {
+                    if (frutasFrescas === "sim") {
+                        drinks.push("Amaro Highball");
+                    } else {
+                        drinks.push("Amaro e Soda");
+                    }
+                } else {
+                    if (frutasFrescas === "sim") {
+                        drinks.push("Amaro Smash");
+                    } else {
+                        drinks.push("Negroni", "Dry Martini", "Old Fashioned", "Manhattan");
+                    }
+                }
+            } else {
+                if (gas === "sim") {
+                    if (frutasFrescas === "sim") {
+                        drinks.push("Aperol Spritz com Frutas");
+                    } else {
+                        drinks.push("Campari Soda");
+                    }
+                } else {
+                    if (frutasFrescas === "sim") {
+                        drinks.push("Vermouth Fruit Cup");
+                    } else {
+                        drinks.push("Vermouth on the Rocks");
+                    }
+                }
+            }
+        }
+    }
+
+    if (sabor === "neutro") {
+        if (citrico === "sim") {
+            if (forcaBebida === "forte") {
+                if (gas === "sim") {
+                    if (frutasFrescas === "sim") {
+                        drinks.push("Gin Fizz com Frutas");
+                    } else {
+                        drinks.push("Tequila Highball");
+                    }
+                } else {
+                    if (frutasFrescas === "sim") {
+                        drinks.push("Daiquiri com Frutas");
+                    } else {
+                        drinks.push("Fitzgerald");
+                    }
+                }
+            } else {
+                if (gas === "sim") {
+                    if (frutasFrescas === "sim") {
+                        drinks.push("Spritz de Vinho Branco com Frutas");
+                    } else {
+                        drinks.push("Moscow Mule", "Tom Collins");
+                    }
+                } else {
+                    if (frutasFrescas === "sim") {
+                        drinks.push("White Wine Sangria")
+                    } else {
+                        drinks.push("Vodka Gimlet");
+                    }
+                }
+            }
+        } else {
+            if (forcaBebida === "forte") {
+                if (gas === "sim") {
+                    if (frutasFrescas === "sim") {
+                        drinks.push("Gin Tônica");
+                    } else {
+                        drinks.push("Whiskey Highball");
+                    }
+                } else {
+                    if (frutasFrescas === "sim") {
+                        drinks.push("Brandy Smash");
+                    } else {
+                        drinks.push("Bloody Mary", "Long Island Iced Tea", "Whiskey Sour");
+                    }
+                }
+            } else {
+                if (gas === "sim") {
+                    if (frutasFrescas === "sim") {
+                        drinks.push("Wine Spritzer");
+                    } else {
+                        drinks.push("Vodka Soda");
+                    }
+                } else {
+                    if (frutasFrescas === "sim") {
+                        drinks.push("Wine Cooler com Frutas");
+                    } else {
+                        drinks.push("Rum Punch", "White Russian", "Black Russian");
                     }
                 }
             }
