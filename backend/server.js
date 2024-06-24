49046eb2c58a01b76b41181a71c6c68b9528ef82
BrendaDantas <brendasod98@gmail.com>
2024-06-24 17:22:10 -0300

    Implementando a tela de registro e login

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,23 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
 const usersRoutes = require('./routes/users');
 
 const app = express();
 const PORT = 3000;
 
+
 app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, '../frontend')));
 app.use('/api/users', usersRoutes);
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/index.html'));
+});
+
+app.get('/diagnostic', (req, res) => {
+    res.send('Diagnostic route reached');
+});
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
